caab4f31c3f3a7aab89e0e7c6fcd21273651ec8d
horb01 <david.horby@springer.com>
2018-12-07 15:06:08 +0000

    Add gradlew

--- src/main/kotlin/com/dhorby/kotlin/cooking/StartCooking.kt
+++ src/main/kotlin/com/dhorby/kotlin/cooking/StartCooking.kt
@@ -1,15 +1,14 @@
 package com.dhorby.kotlin.cooking
 
 import com.dhorby.kotlin.cooking.domain.Dish
-import com.dhorby.kotlin.cooking.domain.Ingredient
+import com.dhorby.kotlin.cooking.domain.Ingredient.*
 import com.dhorby.kotlin.cooking.domain.and
 import com.dhorby.kotlin.cooking.domain.containing
 import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
 
 
-
 fun main(args: Array<String>) = runBlocking {
     val cookingFixedTheadPool = newFixedThreadPoolContext(5, "cooking")
-    val dish = Dish() containing Ingredient.Potato and Ingredient.Cheese and Ingredient.Beans
+    val dish = Dish() containing Potato and Cheese and Beans
 }

5f379042eab360281d2b9cdaec5bd2c5fc5c704f
horb01 <david.horby@springer.com>
2019-01-11 16:26:45 +0000

    Add fuzzy matching

--- src/main/kotlin/com/dhorby/kotlin/text/FuzzyMatcherImpl.kt
+++ src/main/kotlin/com/dhorby/kotlin/text/FuzzyMatcherImpl.kt
@@ -4,30 +4,91 @@ import org.apache.commons.text.similarity.FuzzyScore
 import org.apache.commons.text.similarity.LevenshteinDistance
 import java.util.*
 
+data class FuzzyMatch(val keyVal:Int, val origVal:String, val checkText:String) {
+    val fuzzyScoreer = FuzzyScore(Locale.ENGLISH)
+    val fuzzyScoreVal:Int = fuzzyScoreer.fuzzyScore(origVal.toLowerCase(), checkText.toLowerCase())
+    val levenshteinDistance:Int =  LevenshteinDistance.getDefaultInstance().apply(origVal.toLowerCase(), checkText.toLowerCase())
+    override fun toString(): String {
+        return "${keyVal}, $origVal, $checkText, $fuzzyScoreVal, $levenshteinDistance"
+    }
+}
+
 
 class FuzzyMatcherImpl(val checkList: Map<Int, String>) : FuzzyMatcher {
 
-    val cleanList: Map<Int, String> = checkList.map { it.key to it.value.split(" ").filter { !stopWordList.contains(it) }.joinToString(" ") }.toMap()
+    val cleanList: Map<Int, String> = checkList.map { it.key to it.value.split("\\W+".toRegex()).filter { !stopWordList.contains(it.toLowerCase()) }.joinToString(" ") }.toMap()
+
 
     companion object {
-        val stopWordList = listOf("University", "College", "of")
-
+        val stopWordList = listOf("university", "college", "school", "institute")
     }
+
+//    fun sumLevenshteinDistance(testString: String, compareString: String): Int {
+//        val testList = testString.split("\\W+".toRegex())
+//        val compareList = compareString.split("\\W+".toRegex())
+//        return testList.flatMap {
+//            compareList.map { compareWord -> getValidLevenshteinDistance(it, compareWord) }
+//        }.sum()
+//    }
+
+    fun stopWordFilter(checkText:String): List<String> {
+        return stopWordList.filter { checkText.toLowerCase().contains(it) }
+    }
+
+    fun sumLevenshteinDistance(testString: String, compareString: String): Int {
+        return getValidLevenshteinDistance( testString, compareString)
+    }
+
+
+    fun getValidLevenshteinDistance(testWord:String,compareWord:String):Int  {
+        val result = LevenshteinDistance.getDefaultInstance().apply(testWord.toLowerCase(), compareWord.toLowerCase())
+        println("Comparing " + testWord + " to " + compareWord + " = " + result )
+        return result
+
+//        val final = if (result < testWord.length && result < compareWord.length)
+//             result
+//        else  0
+//        println("Comparing " + testWord + " to " + compareWord + " = " + result + " return= " + final)
+//        return final
+    }
+
     override fun levenshteinDistance(checkText: String): String? {
-        val cleanedText = checkText.split(" ").filter { !stopWordList.contains(it) }.joinToString(" ")
-        println("Cleaned text " + cleanedText)
-        val checkMap = cleanList.map { text -> LevenshteinDistance.getDefaultInstance().apply(text.value, cleanedText)!! to text }.toMap()
-        checkMap.forEach {println("${it.key}:${it.value}")}
-        val mostLikelyMatch = checkMap.minBy { it ->  it.key }
-        val matchedMapValue = checkList.get(mostLikelyMatch!!.value.key)
+        val cleanedText = checkText.split(" ").filter { !stopWordList.contains(it.toLowerCase()) }.joinToString(" ")
+        val checkMap = cleanList.map { it.key to sumLevenshteinDistance(it.value, cleanedText) }.toMap()
+        val mostLikelyMatch = checkMap.minBy { it ->  it.value }
+        val matchedMapValue = checkList.get(mostLikelyMatch!!.key)
         return matchedMapValue
     }
 
     fun fuzzyScore(checkText: String): String? {
         val fuzzyScore = FuzzyScore(Locale.ENGLISH)
-        val checkMap = cleanList.map { text -> fuzzyScore.fuzzyScore(text.value, checkText)!! to text }
-        val mostLikelyMatch = checkMap.minBy { it ->  it.first }
-        val matchedMapValue = checkList.get(mostLikelyMatch!!.first)
+        val checkMap = checkList.map { it.key to fuzzyScore.fuzzyScore(it.value.toLowerCase(), checkText.toLowerCase())}.toMap()
+        val topResults = checkMap.
+                filter { it.value > 5 }
+        val finalMap = topResults.map { checkList.get(it.key) to it.value }
+        finalMap.forEach { println(it.first + ":" + it.second) }
+        val mostLikelyMatch = checkMap.maxBy { it ->  it.value }
+        val matchedMapValue = checkList.get(mostLikelyMatch!!.key)
         return matchedMapValue
+    }
+
+    fun fuzzyAndLevensteinScore(checkText: String):String {
+
+        val fuzzyChecKList = checkList.map { FuzzyMatch(it.key, it.value, checkText) }
+
+        val filterList = stopWordFilter(checkText)
+        val realCheckList = if (filterList.size > 0) {
+            fuzzyChecKList.filter { filterList.filter { filterval -> it.origVal.contains(filterval) }.size == 0 }
+        } else {
+            fuzzyChecKList
+        }
+
+
+
+        val firstFilter = realCheckList.filter { it.fuzzyScoreVal > 6 }
+        firstFilter.forEach { println(it) }
+//        firstFilter.filter { it.levenshteinDistance < 20 }.forEach { println(it) }
+        val mostLikelyMatch = firstFilter.maxBy { it.fuzzyScoreVal }
+        return mostLikelyMatch?.origVal.orEmpty()
     }
 }
--- src/test/kotlin/com/dhorby/kotlin/text/FuzzyMatcherImplTest.kt
+++ src/test/kotlin/com/dhorby/kotlin/text/FuzzyMatcherImplTest.kt
@@ -6,31 +6,44 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class FuzzyMatcherImplTest {
 
-
     val testList = mapOf(
             1 to "Oxford University",
             2 to "Cambridge University",
             3 to "Bristol University",
             4 to "Harvard College")
 
+    val completeList = FuzzyMatcherImplTest::class.java.getResource("/data/entityids.csv").openStream().reader().readLines()
+
+
+    var counter = 0;
+    val mappedCompleteList = completeList.map { counter++ to it  }.toMap()
+
     @Test
-    fun `the word 'ofxord' should match to 'Oxford University'`() {
-        val fuzzyMatcherImpl = FuzzyMatcherImpl(testList)
-        val matchedValue = fuzzyMatcherImpl.levenshteinDistance("ofxord")
-        assertTrue(matchedValue == "Oxford University", "Matched 'ofxord' to $matchedValue")
+    fun `the word 'ofxord university' should match to 'Oxford University'`() {
+        val fuzzyMatcherImpl = FuzzyMatcherImpl(mappedCompleteList)
+        val matchedValue = fuzzyMatcherImpl.fuzzyAndLevensteinScore("oxford university")
+        assertTrue(matchedValue == "University of Oxford", "Matched 'ofxord university' to $matchedValue")
     }
 
     @Test
-    fun `the word 'ambriddeg univesirty' should match to 'Cambridge University'`() {
-        val fuzzyMatcherImpl = FuzzyMatcherImpl(testList)
-        val matchedValue = fuzzyMatcherImpl.levenshteinDistance("ambriddeg univesirty")
-        assertTrue(matchedValue == "Cambridge University", "Matched 'ambriddeg univesirty' to $matchedValue")
+    fun `the word 'ambridge univesirty' should match to 'Cambridge University'`() {
+        val fuzzyMatcherImpl = FuzzyMatcherImpl(mappedCompleteList)
+        val matchedValue = fuzzyMatcherImpl.fuzzyAndLevensteinScore("ambridge university")
+        assertTrue(matchedValue == "University of Cambridge", "Matched 'ambridge university' to $matchedValue")
     }
 
     @Test
-    fun `the word 'University of Cambridge' should match to 'Cambridge University'`() {
-        val fuzzyMatcherImpl = FuzzyMatcherImpl(testList)
-        val matchedValue = fuzzyMatcherImpl.levenshteinDistance("University of Cambridge")
-        assertTrue(matchedValue == "Cambridge University", "Matched 'University of Cambridge' to $matchedValue")
+    fun `the word 'Cambridge University' should match to 'University of Cambridge'`() {
+        val fuzzyMatcherImpl = FuzzyMatcherImpl(mappedCompleteList)
+        val matchedValue = fuzzyMatcherImpl.fuzzyAndLevensteinScore("Cambridge University")
+        assertTrue(matchedValue == "University of Cambridge", "Matched 'University of Cambridge' to $matchedValue")
     }
+
+    @Test
+    fun `the word 'Monfoyt' should match to 'Cambridge University'`() {
+        val fuzzyMatcherImpl = FuzzyMatcherImpl(mappedCompleteList)
+        val matchedValue = fuzzyMatcherImpl.fuzzyAndLevensteinScore("De Montfoyt")
+        assertTrue(matchedValue == "De Montfort University Library Resources", "Matched 'De Montfoyt' to $matchedValue")
+    }
+
 }

bdb7a14a2ce7325a17ad1ab93110b157436ef657
David Horby <david.horby@springernature.com>
2021-10-30 10:39:22 +0100

    Add application plugin

--- src/main/kotlin/com/dhorby/kotlin/web/CookingApp.kt
+++ src/main/kotlin/com/dhorby/kotlin/web/CookingApp.kt
@@ -6,6 +6,12 @@ import org.http4k.cloudnative.env.Environment
  * Main entry point. Note that this will not run without setting up the correct environmental variables
  * - see RunnableEnvironment for a demo-able version of the server.
  */
-fun main() {
-    CookingServer(Environment.ENV).start()
+
+object CookingApp {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        CookingServer(Environment.ENV).start()
+    }
+
 }

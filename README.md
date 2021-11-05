387c853f5b969608916c311b5a3cce38fbeff81d
David Horby <david.horby@springernature.com>
2021-11-05 11:37:06 +0000

    Add docker-compose.yml

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Inspired by the blog:
 
 <a href="https://codeascraft.com/2018/04/12/sealed-classes-opened-my-mind/">Sealed classes opened my mind</a>   
 
+## Dependencies
+brew install --cask temurin
+
+
+## Build
+./gradlew clean build
+
+## Build
+./gradlew run
+
 ## Swagger
 
 http://localhost:8080/openapi/index.html
--- src/main/kotlin/com/dhorby/kotlin/cooking/StartCooking.kt
+++ src/main/kotlin/com/dhorby/kotlin/cooking/StartCooking.kt
@@ -2,6 +2,7 @@ package com.dhorby.kotlin.cooking
 
 import com.dhorby.kotlin.cooking.domain.Dish
 import com.dhorby.kotlin.cooking.domain.Ingredient.*
+import com.dhorby.kotlin.cooking.domain.`cook at`
 import com.dhorby.kotlin.cooking.domain.and
 import com.dhorby.kotlin.cooking.domain.containing
 import kotlinx.coroutines.newFixedThreadPoolContext
@@ -10,6 +11,9 @@ import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) = runBlocking {
     val cookingFixedTheadPool = newFixedThreadPoolContext(5, "cooking")
-    val dish = Dish() containing Potato and Cheese and Beans
+    val dish: Dish = Dish() containing Potato and Cheese and Beans
+    val dish1 = dish `cook at` 200
+    println(dish)
+    println(dish1)
 }
 
